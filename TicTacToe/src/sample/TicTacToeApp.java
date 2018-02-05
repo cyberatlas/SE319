f70a329b0c6e2ec89a910cf5293c63b3ebfd8f2d
unknown <ruski550@gmail.com>
2018-02-04 23:57:22 -0600

    Now displays images

--- TicTacToe/src/sample/TicTacToeApp.java
+++ TicTacToe/src/sample/TicTacToeApp.java
@@ -1,4 +1,4 @@
-package sample;
+//package sample;
 
 import javafx.application.Application;
 import javafx.geometry.Pos;
@@ -11,6 +11,7 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.scene.image.*;
 
 
 
@@ -20,18 +21,18 @@ public class TicTacToeApp extends Application {
     private boolean isXturn = true;
     private boolean ableToPlay = true;
 
-    private Parent createContent(){
+    private Parent createContent() {
         Pane root = new Pane();
         root.setPrefSize(600, 600);
 
         //Creates a 3x3 grid
-        for (int i =0; i<3; i++) {
-            for (int j =0; j< 3; j++){
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
 
                 //Creates the tile and each time changes the x and y axis
                 Tile tile = new Tile();
-                tile.setTranslateX(j*200);
-                tile.setTranslateY(i*200);
+                tile.setTranslateX(j * 200);
+                tile.setTranslateY(i * 200);
 
                 root.getChildren().add(tile);
 
@@ -51,11 +52,16 @@ public class TicTacToeApp extends Application {
     }
 
     //Each of the squares on the grid
-    private class Tile extends StackPane{
+    private class Tile extends StackPane {
         //TODO find out how to replace that with an image
         private Text text = new Text();
+        private Image ximage = new Image("x.jpg");
+        private Image oimage = new Image("o.jpg");
 
-        public Tile(){
+        private ImageView imageView = new ImageView();
+
+
+        public Tile() {
             Rectangle border = new Rectangle(200, 200);
             //By default the squares are black, need to set the fill to none
             border.setFill(null);
@@ -64,13 +70,14 @@ public class TicTacToeApp extends Application {
             //Aligns everything to the center of the page
             setAlignment(Pos.CENTER);
             //TODO will need to figure out how to replace that with an image
-            getChildren().addAll(border, text);
+//            getChildren().addAll(border, text);
+            getChildren().addAll(border, imageView);
 
-            setOnMouseClicked(event ->{
+            setOnMouseClicked(event -> {
                 //On left mouse click. TODO Will need to change that for alternating turns.
                 if (event.getButton() == MouseButton.PRIMARY) {
 
-                    if(isXturn == true){
+                    if (isXturn == true) {
 
                         drawX();
                         isXturn = false;
@@ -83,23 +90,32 @@ public class TicTacToeApp extends Application {
                         return;
                     }
                 }
-            } );
+            });
 
-            }
+        }
 
-            //TODO change it so that it overlays the X image needed for the project instead
-            private void drawX(){
-                text.setText("X");
-            }
-            //TODO change it so that it overlays the Y image needed for the project instead
-            private void drawO(){
-                text.setText("O");
-    }       }
+        //TODO change it so that it overlays the X image needed for the project instead
+        private void drawX() {
+//                text.setText("X");
+            imageView.setImage(ximage);
+            imageView.setFitWidth(200);
+            imageView.setFitHeight(200);
+        }
 
-    public static void main(String[] args) {
+        //TODO change it so that it overlays the Y image needed for the project instead
+        private void drawO() {
+//                text.setText("O");
+            imageView.setImage(oimage);
+            imageView.setFitWidth(200);
+            imageView.setFitHeight(200);
 
-        launch(args);
-
-
+        }
     }
-}
+
+        public static void main(String[] args) {
+
+            launch(args);
+
+
+        }
+    }
